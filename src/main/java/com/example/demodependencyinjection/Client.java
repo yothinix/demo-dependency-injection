1aec8fe3752082c81fbb15fa75f397fcdf821e97
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 11:14:57 +0700

    :sparkles: Add class Client, Hello :rocket:

---
+++ src/main/java/com/example/demodependencyinjection/Client.java
@@ -0,0 +1,7 @@
+package com.example.demodependencyinjection;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/demodependencyinjection/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demodependencyinjection;
+
+public class Hello {
+
+    private String message = "Default message";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
