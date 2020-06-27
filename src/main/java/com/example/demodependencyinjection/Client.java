eb9a2870509e1afcb3620aa87415c2c12f477295
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 11:19:37 +0700

    :sparkles: Add constructor injection :rocket:

--- src/main/java/com/example/demodependencyinjection/Client.java
+++ src/main/java/com/example/demodependencyinjection/Client.java
@@ -3,10 +3,11 @@ package com.example.demodependencyinjection;
 public class Client {
     public static void main(String[] args) {
 
-        Hello hello = new Hello();
+//        Hello hello = new Hello(); // No Injection
+        Hello hello = new Hello("John"); // Constructor Injection
         String message = hello.getMessage();
 
-        System.out.println("Hello, My name is" + message);
+        System.out.println("Hello, My name is " + message);
 
     }
 }
--- src/main/java/com/example/demodependencyinjection/Hello.java
+++ src/main/java/com/example/demodependencyinjection/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
     private String message = "Default message";
 
+    public Hello(String message) {
+        this.message = message;
+    }
+
     public String getMessage() {
         return message;
     }
