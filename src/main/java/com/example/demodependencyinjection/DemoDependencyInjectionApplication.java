aef336f8aed3bf6dfcb5540f04fb6e156d60108b
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 11:12:30 +0700

    :tada: Initial project :rocket:

---
+++ src/main/java/com/example/demodependencyinjection/DemoDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDependencyInjectionApplication.class, args);
+	}
+
+}
