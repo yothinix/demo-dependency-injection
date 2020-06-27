42f6392c23249bae9db61c1ba6da38ad116eedf6
Yothin Muangsommuk <m@yothinix.com>
2020-06-27 11:17:05 +0700

    :sparkles: Add boiler plate for client class :rocket:

--- src/main/java/com/example/demodependencyinjection/Client.java
+++ src/main/java/com/example/demodependencyinjection/Client.java
@@ -3,5 +3,10 @@ package com.example.demodependencyinjection;
 public class Client {
     public static void main(String[] args) {
 
+        Hello hello = new Hello();
+        String message = hello.getMessage();
+
+        System.out.println("Hello, My name is" + message);
+
     }
 }
